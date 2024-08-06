5288b7008771ccec52e166425d4623cc91d9dd4d
abhirupinspace <abkenway2003@gmail.com>
2024-08-07 02:03:09 +0530

    Changing Anon Aadhar

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App"; // Default import
+import App from "./App";
 import { ThirdwebProvider } from "thirdweb/react";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Profile from "./pages/profile";
+
+import PaymentPage from "./pages/PaymentPage";
+import Profile from "./pages/Profile";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
                 <Routes>
                     <Route path='/' element={<App />} />
                     <Route path='/profile' element={<Profile />} />
+                    <Route path='/payments' element={<PaymentPage />} />
                 </Routes>
             </BrowserRouter>
     </ThirdwebProvider>
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/Navbar'
 
 const profile = () => {
     return (
         <div>
-            
+            <Navbar />
+            <h1>profile page</h1>
         </div>
     )
 }
